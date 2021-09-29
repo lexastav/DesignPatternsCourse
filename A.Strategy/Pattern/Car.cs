356dcf5828ee0a782a382f23a712fa9760681227
Alexey Volegov <anvolegov87@gmail.com>
2021-09-29 16:28:32 +0300

    studied Strategy pattern

---
+++ A.Strategy/Pattern/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A.Strategy.Pattern
+{
+    class Car
+    {
+        protected int passengers;
+        protected string model;
+
+        public Car(int passengers, string model, IEngineTypeble typeEngine)
+        {
+            this.passengers = passengers;
+            this.model = model;
+            EngineTypeble = typeEngine;
+        }
+        public IEngineTypeble EngineTypeble { private get; set; }
+        public void TypeEngine()
+        {
+            EngineTypeble.TypeEngine();
+        }
+    }
+}
---
+++ A.Strategy/Pattern/ElectricType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A.Strategy.Pattern
+{
+    class ElectricType : IEngineTypeble
+    {
+        public void TypeEngine()
+        {
+            Console.WriteLine("Ездит на электричесстве");
+        }
+    }
+}
